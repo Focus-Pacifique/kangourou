a0d61bd509dc5669a75d87d62d18bf0fe539ee9e
ManooshMol <molle.alexis@gmail.com>
2013-10-16 19:00:30 +0200

    Ajout de l'upload
    Dans le dossier : home/downloads/USA_2013

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,13 @@
 package controllers;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Date;
 import java.util.List;
 
@@ -9,15 +17,16 @@ import models.Saison;
 import models.Sys_parameter;
 import models.Utilisateur;
 import play.mvc.Controller;
+import play.mvc.Http.MultipartFormData.FilePart;
 import play.mvc.Result;
 import play.mvc.Security.Authenticated;
 import views.html.index;
+import views.html.upload;
 
 @Authenticated(Secured.class)
 public class Application extends Controller {
 
-  
-	  public static Result index() {
+    public static Result index() {
 		  Date maintenant = new Date();
 		  List<Matche> prochainMatchs = Matche.find.where().gt("dateMatche", maintenant).orderBy().asc("dateMatche").findList();
 		  Matche prochainMatch=new Matche();
@@ -35,6 +44,41 @@ public class Application extends Controller {
 		  return ok(index.render(user,pointsSaisons,prochainMatch,points)
 				  );
 		  
+	  }
+	  	  
+	  public static Result upload() throws IOException {
+		  play.mvc.Http.MultipartFormData body = request().body().asMultipartFormData();
+		  FilePart picture = body.getFile("picture");
+		  if (picture != null) {
+		    String fileName = picture.getFilename();
+		    String contentType = picture.getContentType(); 
+		    File file = picture.getFile();
+		    
+		        InputStream is = new FileInputStream(file);
+		        BufferedInputStream bis = new BufferedInputStream(is);
+		        OutputStream os = new BufferedOutputStream(new FileOutputStream("/home/transmission/USA_2013" + fileName));
+		        BufferedOutputStream bos = new BufferedOutputStream(os);
+		        byte[] buffer = new byte[4096];
+		        int readCount;
+		        while( (readCount = bis.read(buffer)) > 0)
+		        {
+		              bos.write(buffer, 0, readCount);
+		        }
+		        bos.close();
+
+
+		    return redirect(routes.Application.upload());
+		  } else {
+		    flash("error", "Missing file");
+		    return redirect(routes.Application.index());
+		  }
+		}
+	  public static Result affiche() {
+		  Utilisateur user = Utilisateur.findByPseudo(request().username());
+		  Sys_parameter system = Sys_parameter.find.byId((long) 1);
+		  Saison saison = system.getSaisonEnCours();
+		  Integer points = PointsSaison.find.where().eq("saison", saison).eq("user", user).findList().get(0).pointsTotalSaison;
+		  return ok(upload.render(user, points));
 	  }
   
 }
